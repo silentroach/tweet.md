95a70d0ca7fe9af2285d505dede999ee8937ff8c
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-08-27 14:20:04 +0200

    proper markdown br

--- test/fixtures/multiline.js
+++ test/fixtures/multiline.js
@@ -2,4 +2,5 @@ export const input = {
 	text: 'Something with multiple\nlines'
 }
 
-export const output = 'Something with multiple<br />lines';
+export const output = `Something with multiple  
+lines`;
